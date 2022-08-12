69aaa87ec4bab1114c9f2532ccc3e6704fb44702
Xu Xun <xu_xun@i2r.a-star.edu.sg>
2022-08-12 10:58:20 +0800

    update

--- ProjectPage/ICIP_15/Demo/Code/function/func_NormalizeFeatureMatrix.m
+++
@@ -1,5 +0,0 @@
-%% Normalize Feature Matrix
-
-function normFeatureMatrix = func_NormalizeFeatureMatrix(FeatureMatrix)
-
-normFeatureMatrix = FeatureMatrix./repmat(sum(FeatureMatrix,2),1,size(FeatureMatrix,2));
